352f0b58d5bdc6cddaef485ea83eeffa33b8df8e
Andre Luis <andre.1357.luis@gmail.com>
2022-09-27 23:32:31 -0300

    ✨ add util

---
+++ src/utils/index.js
@@ -0,0 +1,40 @@
+export default class Util {
+  /**
+   *
+   * @param {Object}  args
+   * @param {string}  args.str - string to transform
+   * @param {boolean} args.upperCase - if true, transform to PascalCase, otherwise to camelCase
+   * @returns {string}
+   */
+  static #transform({ str, upperCase }) {
+    if (!str.trim().length) {
+      return '';
+    }
+
+    if (upperCase) {
+      return str.charAt(0).toUpperCase().concat(str.slice(1));
+    }
+
+    return str.charAt(0).toLowerCase().concat(str.slice(1));
+  }
+
+  /**
+   * transform a string to PascalCase
+   *
+   * @param {string} str
+   * @returns {string}
+   */
+  static upperCaseFirstLetter(str) {
+    return Util.#transform({ str, upperCase: true });
+  }
+
+  /**
+   * transform a string to camelCase
+   *
+   * @param {string} str
+   * @returns {string}
+   */
+  static lowerCaseFirstLetter(str) {
+    return Util.#transform({ str, upperCase: false });
+  }
+}
